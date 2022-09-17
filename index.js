3ca0328fd42392d6daed0c899d2a286b207f1d91
YinHk <ministudiowingyin@gmail.com>
2022-09-17 01:47:47 -0400

    update

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const PORT = 3000;
+
+const app = express();
+app.use();
+
+app.listen(PORT, () => 
+    console.log(`The server is running on: http://localhost:${PORT}`));
+
+
+    
